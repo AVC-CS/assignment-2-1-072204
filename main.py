df7a48a9a322e85a3ddf3a58d8bc35e472fa2f1e
maya <157551581+072204@users.noreply.github.com>
2024-02-24 05:52:26 +0000

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 def main():
-    """
-    ##################################################
-    Comlete your code here
-    Use m_perc and f_perc for your results
-    ##################################################
-    """
+    
+    m = int(input('Enter the number of male students: ')) 
+    f = int(input('Enter the number of female students: ')) 
 
-    """
-    ########################################
-    # Do not delete the return statement
-    ########################################
-    """
+    total = m + f
+    m_perc = m / total * 100
+    f_perc = f / total * 100
+
+    print(f'The total number of students is: \t \t {total}')
+    print(f'The number of males and females is:  \t \t {m_perc}\t {f_perc}')
+    print(f'The percentage of males and females is:  \t {m_perc:.2f}% {f_perc:.2f}%')
+
+
     return m_perc, f_perc
 
 
