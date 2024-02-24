79a22ea5dc49f4f83589140b82e2c4f137036bd5
maya <157551581+072204@users.noreply.github.com>
2024-02-24 06:27:18 +0000

    DONE!

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     f_perc = f / total * 100
 
     print(f'The total number of students is: \t \t {total}')
-    print(f'The number of males and females is:  \t \t {m_perc}\t {f_perc}')
+    print(f'The number of males and females is:  \t \t {m}\t{f}')
     print(f'The percentage of males and females is:  \t {m_perc:.2f}% {f_perc:.2f}%')
 
 
